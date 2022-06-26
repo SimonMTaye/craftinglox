fe276c38ad5c161311231041b818b268dd5970c4
SimonMTaye <mulat.simon@gmail.com>
2022-06-25 19:41:52 -0700

    Commit generator functions

---
+++ jlox/tools/__main__.py
@@ -0,0 +1,95 @@
+import os
+from os import path, makedirs
+from sys import argv
+from typing import Dict, Tuple, List
+
+from .data_class import DerivedClass, get_base_class
+
+# Package string
+ROOT = r"C:\Users\smtsi\Code\interpreter\jlox"
+Pair = Tuple[str, str]
+
+
+def output(source: str) -> None:
+    rem_lines = []
+    base_n = ""
+    imports = ""
+    package = ""
+    nested_dir = ""
+    with open(source, "r") as src_file:
+        base_n = src_file.readline()
+        base_n = base_n.strip(" \n")
+        lines = src_file.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith("$"):
+                package += line.strip("$\n")
+                package += "\n"
+            elif line.startswith("*"):
+                imports += line.strip("*\n")
+                imports += "\n"
+            elif line.startswith("!"):
+                nested_dir = line.strip("!\n")
+                continue
+            else:
+                rem_lines = lines[i:]
+                break
+    generate_output(f"{ROOT}\\{nested_dir}", package, imports, base_n, "Visitor",
+                    rem_lines)
+
+
+def visitor_interface(package: str, vistor_name: str,
+                      class_names: List[str]) -> str:
+    v_str = f"{package}\n\npublic interface {vistor_name}<R> "
+    v_str += "{\n"
+    for name in class_names:
+        v_str += f"\tpublic R visit{name}({name} {name.lower()});\n"
+    v_str += "}"
+    return v_str
+
+
+def generate_output(output_dir: str, package: str, imports: str, base: str,
+                    visitor: str, rem_lines: List[str]):
+    makedirs(output_dir, exist_ok=True)
+    vistor_name = f"{base}{visitor}"
+    with open(path.join(output_dir, f"{base}.java"), "w") as base_file:
+        base_file.write(get_base_class(base, vistor_name, package))
+    class_dict = parse_output(rem_lines)
+    for key in class_dict.keys():
+        out_path = path.join(output_dir, f"{key}.java")
+        expression = DerivedClass(base, key,  package, imports, vistor_name)
+        expression.add_fields(class_dict[key])
+        with open(out_path, "w") as out_file:
+            out_file.write(expression.to_string())
+    class_names = [key for key in class_dict.keys()]
+    visitor_path = path.join(output_dir, f"{vistor_name}.java")
+    with open(visitor_path, "w") as visitor_file:
+        visitor_file.write(visitor_interface(package, vistor_name, class_names))
+
+
+def parse_output(lines: List[str]) -> Dict[str, List[Pair]]:
+    parsed_lines = []
+    for line in lines:
+        parsed_lines.append(parse_line(line))
+    return {line[0]: line[1] for line in parsed_lines}
+
+
+def parse_line(line: str) -> Tuple[str, List[Pair]]:
+    words = line.split(" ")
+    assert len(words) % 2 != 0
+    pairs = [(words[i + 1].strip(), words[i].strip()) for i in
+             range(1, len(words), 2)]
+    return words[0].strip(), pairs
+
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        output(argv[1])
+    else:
+        root = f"{os.getcwd()}/tools/packages"
+        files = os.listdir(root)
+        for fp in files:
+            if fp.endswith(".txt"):
+                output(f"{root}/{fp}")
+
+
+
---
+++ jlox/tools/data_class.py
@@ -0,0 +1,107 @@
+from typing import Dict, Tuple, List, Optional
+
+
+def get_base_class(name: str, visitor: str, package: str) -> str:
+    so_far = package + "\n\n"
+    so_far += "public abstract class " + name + " {\n"
+    so_far += "\tpublic abstract <R> R accept(" + visitor + "<R> v);\n}"
+    return so_far
+
+
+Pair = Tuple[str, str]
+
+def get_field_str(name: str, type_str: str) -> str:
+    """Get a string output for a defined field"""
+    return f"\tpublic final {type_str} {name};\n"
+
+
+class DerivedClass:
+    def __init__(self, base: str, name: str, package: str, imports: str,
+                 visitor: Optional[str] = None) -> None:
+        self.fields: Dict[str, str] = {}
+        self.__name: str = name.strip()
+        self.__base = base.strip()
+        self.__visitor = visitor
+        self.__package = package
+        self.__imports = imports
+
+    @property
+    def has_visitor(self) -> bool:
+
+        return self.__visitor is not None
+
+    @property
+    def name(self) -> str:
+        """Return the name of this class"""
+        return self.__name
+
+    @property
+    def base(self) -> str:
+        """Return the name of the  base class"""
+        return self.__base
+
+    @property
+    def visitor_name(self) -> str:
+        """Return the name of the visitor class"""
+        assert self.__visitor is not None
+        return self.__visitor
+
+    def add_fields(self, pairs: List[Pair]):
+        """Add multiple fields to the generated class. See docs for add_field"""
+        for pair in pairs:
+            self.add_field(pair)
+
+    def add_field(self, pair: Pair) -> None:
+        """
+         Add a field to the generated class
+         Each fields name is determined by the first value in the pair and the
+         type is determined by the second value
+         Each field will be set to private
+         Getters will be generated for each field
+         """
+        self.fields[pair[0]] = pair[1]
+
+    def to_string(self) -> str:
+        """Generate the class as a string"""
+        class_str = self.__package
+        if self.__imports != "":
+            class_str += f"\n\n{self.__imports}"
+        class_str += f"\n\npublic class {self.name} extends {self.base} "
+        class_str += "{\n"
+        # Add the fields to the class
+        # TODO Add getters for fields
+        for key in self.fields.keys():
+            class_str += get_field_str(key, self.fields[key])
+        class_str = DerivedClass.add_line(class_str, self.constructor())
+        if self.has_visitor:
+            class_str = DerivedClass.add_line(
+                class_str, self.visitor(f"{self.visitor_name}")
+            )
+        class_str += "}\n"
+        return class_str
+
+    def constructor(self) -> str:
+        """Get the generated class constructor"""
+        args = [f"{self.fields[key]} {key}, " for key in self.fields.keys()]
+        # public Name (int sfa, ..) {
+        args_str = "".join(args)
+        args_str = args_str.strip(" ,")
+        # Add list of params to constructor
+        my_str = f"\tpublic {self.name} ({args_str}"
+        my_str = my_str + ") {\n"
+        # \t this.stuff = stuff;
+        for key in self.fields.keys():
+            my_str = f"{my_str}\t\tthis.{key} = {key};\n"
+        my_str += "\t}\n"
+        return my_str
+
+    def visitor(self, visitor_name: str) -> str:
+        """Implement the visitor in the generated class"""
+        header = "\tpublic <R> R accept(" + visitor_name + "<R> visitor) {\n"
+        body = f"\t\treturn visitor.visit{self.name}(this);\n"
+        return header + body + "\t}\n"
+
+    @staticmethod
+    def add_line(instr, line):
+        """A line of string + a new line character"""
+        return instr + "\n" + line
